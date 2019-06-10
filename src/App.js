dc8afbf85f3cb9fb000ee8326d3f6b1723f8b082
Brandon Dixon <bdixon850@yahoo.com>
2019-06-10 06:15:16 -0400

    searchworking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Recipes from './components/Recipes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = '76aeaecf34ee2040add5f3dcb6325282';
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+
+    //Make API Call *Prefix localhost with cors-anywhere link for cross origin error*
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
+    
+    //Get API data in JSON format
+    const data = await api_call.json();
+
+    this.setState({recipes: data.recipes});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Foodie</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Form(props) {
     return (
-        <form onSubmit={props.getRecipe} style={{}}>
+        <form onSubmit={props.getRecipe} style={{ marginBottom: '2rem' }}>
             <input type="text" name="recipeName" className="form-input" placeholder="Search for a recipe...." />
         </form>
     )
